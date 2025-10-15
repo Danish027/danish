9136a96c223e0027b983d0e65f726200a0e7119a
Danish027 <danishqes0786@gmail.com>
2025-10-15 21:40:02 +0530

    about me

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
-import type { NextConfig } from "next";
+import withMDX from "@next/mdx";
+import { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+export default withMDX()({
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  redirects: async () => [
+    {
+      source: "/posts/:slug",
+      destination: "/thoughts/:slug",
+      permanent: false,
+    },
+  ],
+  experimental: {
+    viewTransition: true,
+    mdxRs: {
+      mdxType: "gfm",
+    },
+  },
+  transpilePackages: ["shiki"],
+  images: {
+    contentDispositionType: "inline",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+} satisfies NextConfig);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,78 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="max-w-2xl mx-auto">
+      <h1 className="font-semibold mb-8">Mohammed Danish</h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div className="space-y-6 text-base leading-loose text-gray-600">
+        <p>
+          I am a developer at{" "}
+          <Link
+            href="https://seashellpack.com"
+            className="font-medium underline underline-offset-4 hover:text-gray-600 transition-colors"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            Seashell
+          </Link>{" "}
+          and founder of{" "}
+          <Link
+            href="https://invoiceapp.io"
+            className="font-medium underline underline-offset-4 hover:text-gray-600 transition-colors"
           >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            Invoiceapp
+          </Link>
+          . I am passionate about building technology that makes a meaningful
+          impact on people's lives.
+        </p>
+
+        <p>
+          I graduated in <span className="font-medium">2024</span> with a BTech
+          in Computer Science and Engineering. In{" "}
+          <span className="font-medium">2023</span>, I joined Seashell as a
+          Founding Engineer, building a software platform that serves as
+          packaging's three-sided marketplace in the United States. In{" "}
+          <span className="font-medium">2022</span>, I started working on
+          Invoiceapp, a platform to create invoices and track payments. I work
+          with <span className="font-medium">Next.js</span>,{" "}
+          <span className="font-medium">TypeScript</span>,{" "}
+          <span className="font-medium">Tailwind</span>,{" "}
+          <span className="font-medium">tRPC</span>,{" "}
+          <span className="font-medium">React Query</span>, and{" "}
+          <span className="font-medium">Drizzle ORM</span>.
+        </p>
+
+        <p>
+          You can find me on{" "}
+          <Link
+            href="https://github.com/danish027"
+            className="underline underline-offset-4 hover:text-gray-600 transition-colors"
+          >
+            GitHub
+          </Link>
+          ,{" "}
+          <Link
+            href="https://x.com/Danish_027"
+            className="underline underline-offset-4 hover:text-gray-600 transition-colors"
+          >
+            X/Twitter
+          </Link>
+          , and{" "}
+          <Link
+            href="https://www.linkedin.com/in/danish027/"
+            className="underline underline-offset-4 hover:text-gray-600 transition-colors"
+          >
+            LinkedIn
+          </Link>
+          , or reach out via{" "}
+          <Link
+            href="mailto:mohammeddanish.dev@gmail.com"
+            className="underline underline-offset-4 hover:text-gray-600 transition-colors"
+          >
+            email
+          </Link>
+          .
+        </p>
+      </div>
     </div>
   );
 }
