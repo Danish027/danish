a14a139b53000dd7bc02875fdf48c35ee9b10959
mustbemo <danishqes0786@gmail.com>
2026-04-22 14:29:42 +0530

    calligraphy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { One } from "./components/1";
+import { Two } from "./components/2";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div>
+      <One />
+      <Two />
+    </div>
+  );
 }
